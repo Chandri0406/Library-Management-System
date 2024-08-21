ddc6da9f1e0d947741134bdee3bc30398971b898
DMCoder2022 <unidm2022@gmail.com>
2024-08-21 15:14:41 +0200

    Updated Loan Model

--- Desktop/src/desktop/models/Loan.java
+++ Desktop/src/desktop/models/Loan.java
@@ -32,4 +32,44 @@ public class Loan {
 
     public Loan() {
     }
+
+    public int getLoanID() {
+        return LoanID;
+    }
+
+    public int getBookID() {
+        return BookID;
+    }
+
+    public Date getStartDate() {
+        return StartDate;
+    }
+
+    public Date getEndDate() {
+        return EndDate;
+    }
+
+    public int getLibraryCardID() {
+        return LibraryCardID;
+    }
+
+    public void setLoanID(int loanID) {
+        LoanID = loanID;
+    }
+
+    public void setBookID(int bookID) {
+        BookID = bookID;
+    }
+
+    public void setStartDate(Date startDate) {
+        StartDate = startDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        EndDate = endDate;
+    }
+
+    public void setLibraryCardID(int libraryCardID) {
+        LibraryCardID = libraryCardID;
+    }
 }
