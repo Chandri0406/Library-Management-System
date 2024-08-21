86846127a78dd1e6ffca53dac078aca168253ff6
Martin-94 <114762049+Martin-94@users.noreply.github.com>
2024-08-21 20:29:45 +0200

    Database connection

---
+++ Desktop/src/GUI/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection
+{
+private  static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+private static final String JDBC_URL = "jdbc:derby:backup1;create=true";
+Connection con;
+public DatabaseConnection()
+{
+
+}
+public void connect() throws ClassNotFoundException{
+    try{
+        Class.forName(DRIVER);
+        this.con = DriverManager.getConnection(JDBC_URL);
+        if (this.con == null)
+        {
+            System.out.println("Connected to the database");
+        }
+    }
+    catch (SQLException ex)
+    {
+        ex.printStackTrace();
+    }
+}
+}
