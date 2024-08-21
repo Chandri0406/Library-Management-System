66bcd7f42a60b29e77429c47cc37d1bdced6c7b6
DMCoder2022 <unidm2022@gmail.com>
2024-08-21 14:40:56 +0200

    Added Book Model class

---
+++ Desktop/src/desktop/models/Book.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package desktop.models;
+
+/**
+ *
+ * @author Super
+ */
+public class Book {
+    private int BookID;
+    private String Title;
+    private String Genre;
+    private double Price;
+    private String AuthorID;
+
+    public Book(int BookID, String Title, String Genre, double Price, String AuthorID) {
+        this.BookID = BookID;
+        this.Title = Title;
+        this.Genre = Genre;
+        this.Price = Price;
+        this.AuthorID = AuthorID;
+    }
+    
+    public Book(int BookID, String Title, String Genre, double Price) {
+        this.BookID = BookID;
+        this.Title = Title;
+        this.Genre = Genre;
+        this.Price = Price;
+    }
+
+    public int getBookID() {
+        return BookID;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getGenre() {
+        return Genre;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public String getAuthorID() {
+        return AuthorID;
+    }
+
+    public void setBookID(int BookID) {
+        this.BookID = BookID;
+    }
+
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    public void setGenre(String Genre) {
+        this.Genre = Genre;
+    }
+
+    public void setPrice(double Price) {
+        this.Price = Price;
+    }
+
+    public void setAuthorID(String AuthorID) {
+        this.AuthorID = AuthorID;
+    }
+}
