448ebcbddc36bc6435d943a44e5e0a508a46ee4d
DMCoder2022 <unidm2022@gmail.com>
2024-08-23 21:38:04 +0200

    Updated Desktop Models

--- Desktop/src/desktop/models/Book.java
+++ Desktop/src/desktop/models/Book.java
@@ -2,37 +2,41 @@
 package desktop.models;
 
 public class Book {
-    private int BookID;
+    private String BookID;
     private String Title;
     private String Genre;
     private int YearOfPublication;
+    private String Status;
     private String AuthorID;
 
-    public Book(int BookID, String Title, String Genre, int yearOfPublication, String AuthorID) {
+    public Book(String BookID, String title, String Genre, int yearOfPublication, String status, String AuthorID) {
         this.BookID = BookID;
-        this.Title = Title;
+        Title = title;
         this.Genre = Genre;
         YearOfPublication = yearOfPublication;
+        Status = status;
         this.AuthorID = AuthorID;
     }
     
-    public Book(int BookID, String Title, String Genre, int yearOfPublication) {
+    public Book(String BookID, String title, String Genre, int yearOfPublication, String status) {
         this.BookID = BookID;
-        this.Title = Title;
+        Title = title;
         this.Genre = Genre;
         YearOfPublication = yearOfPublication;
+        Status = status;
     }
 
-    public Book(String title, String genre, int yearOfPublication) {
+    public Book(String title, String genre, int yearOfPublication, String status) {
         Title = title;
         Genre = genre;
         YearOfPublication = yearOfPublication;
+        Status = status;
     }
 
     public Book() {
     }
 
-    public int getBookID() {
+    public String getBookID() {
         return BookID;
     }
 
@@ -48,16 +52,20 @@ public class Book {
         return YearOfPublication;
     }
 
+    public String getStatus() {
+        return Status;
+    }
+
     public String getAuthorID() {
         return AuthorID;
     }
 
-    public void setBookID(int BookID) {
+    public void setBookID(String BookID) {
         this.BookID = BookID;
     }
 
-    public void setTitle(String Title) {
-        this.Title = Title;
+    public void setTitle(String title) {
+        Title = title;
     }
 
     public void setGenre(String Genre) {
@@ -66,6 +74,10 @@ public class Book {
 
     public void setYearOfPublication(int yearOfPublication) {
         YearOfPublication = yearOfPublication;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
     }
 
     public void setAuthorID(String AuthorID) {
